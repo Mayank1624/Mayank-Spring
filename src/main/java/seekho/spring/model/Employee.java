72e2f2078edc6b58d23949b964876e48c3b1e57f
Mayank Srivastava <mayankshrivastava1624@gmail.com>
2026-09-06 16:43:21 +0530

    update

--- src/main/java/seekho/spring/model/Employee.java
+++ src/main/java/seekho/spring/model/Employee.java
@@ -1,13 +1,22 @@
 package seekho.spring.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Employee {
 	
 	private int id;
 	private String name, gender;
 	
+	@Autowired
+	@Qualifier(value="add2")
+	private IAddress address;
 	
 	public Employee() {
 		super();
+		id=111;
 		System.out.println("Employee.Employee()");
 	}
 
@@ -18,6 +27,16 @@ public class Employee {
 		this.name = name;
 		this.gender = gender;
 		System.out.println("Employee.Employee(3)");
+	}
+	
+  public IAddress getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(IAddress address) {
+		this.address = address;
+		System.out.println("Employee.setAddress");
 	}
 
 
@@ -56,8 +75,11 @@ public class Employee {
 
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + "]";
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", address=" + address + "]";
 	}
+
+
+	
 	
 	
 	
